62d3262b8664d1901619724dfcb4fbf9f547a588
Nick <nick.woodward@hotmail.co.uk>
2024-11-15 11:51:21 +0000

    altering header width

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { MobileMenu } from "./MobileMenu";
 
 export const Header = () => {
   return (
-    <div className="fixed w-full flex justify-between items-center px-4 h-[3rem] bg-slate-700">
+    <div className="fixed w-screen flex justify-between items-center px-4 h-[3rem] bg-slate-700">
       <div className="text-slate-200">LOGO</div>
       <MobileMenu />
     </div>
